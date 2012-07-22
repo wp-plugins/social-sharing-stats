2ee8074f8e421db408d4e121b2e3621608da3bb1
labnol <labnol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-22 19:38:39 +0000

    First Version - Added by @labnol
    git-svn-id: https://plugins.svn.wordpress.org/social-sharing-stats/trunk@575797 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ labnol-social-sharing.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Plugin Name: Social Sharing Stats
+Plugin URI: http://www.labnol.org/
+Description: Show Shocial Sharing stats for your blog posts in the WordPress Admin Dashboard
+Author: Amit Agarwal
+Version: 0.1
+Author URI: http://www.labnol.org/
+*/
+
+function add_labnol_column($columns) {
+  return array_merge( $columns, 
+           array('labnolsocial' => __('Social Stats')) );
+}
+
+function social_enqueue($hook) {
+    if( 'edit.php' != $hook )
+        return;
+    wp_enqueue_script( 'social_script', 'http://s7.addthis.com/js/250/addthis_widget.js#pubid=labnol', array(), false, true  );
+}
+add_action( 'admin_enqueue_scripts', 'social_enqueue' );
+
+function labnol_columns( $column, $post_id ) {
+  if ( $column == "labnolsocial" ) :
+?>
+ <div class="addthis_toolbox addthis_default_style" 
+    addthis:url="<?php echo get_permalink ( $post_id ); ?>" 
+    addthis:title="<?php echo htmlentities(get_the_title($post_id)); ?>">
+   <a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
+   <a class="addthis_button_tweet"></a>
+   <a class="addthis_button_google_plusone" g:plusone:size="medium"></a>
+ </div>
+<?php
+ endif; 
+}
+
+add_filter('manage_posts_columns' , 'add_labnol_column');
+add_action('manage_posts_custom_column' , 'labnol_columns');
+add_filter('manage_pages_columns' , 'add_labnol_column');
+add_action('manage_pages_custom_column' , 'labnol_columns');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== Social Sharing Stats ===
+Tags: twitter, facebook, tweet, google, social, admin
+Contributors: labnol
+Requires at least: 3.1
+Tested up to: 3.4.1
+Stable tag: trunk
+
+Display the social sharing counts of your blog posts in the WordPress Admin dashboard.
+
+== Description ==
+
+The Social Sharing plugin will display the the social sharing counts of your blog posts in the WordPress Admin dashboard. It adds a new column to your Edit Posts screen where these numbers are displayed.
+
+The statistics are pulled live from Twitter, Facebook, Google Plus and other social networks. You can also tweet, like or +1 your blog posts from the WordPress admin dashboard itself.
+
+If you would like to hide the Social Sharing Stats column in WordPress, either choose Screen Options and uncheck the Social column or deactivate the plugin itself.
+
+The plugin uses JavaScript to display the social sharing counts and does not make any changes to your WordPress database or blog posts. For best performance, display 10-20 posts per WordPress screenshot.
+
+== Screenshots ==
+
+1. Social Sharing Stats
+
+== Installation ==
+
+Install the Social Sharing plugin either via the WordPress.org plugin directory, or by uploading the files to your server
